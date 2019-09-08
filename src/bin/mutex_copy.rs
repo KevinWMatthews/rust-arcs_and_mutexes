b32c1a5323527bbc35825ea7299ddb95fa2a7f38
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-08 08:07:38 -0700

    Add basic example of mutexes

---
+++ src/bin/mutex_copy.rs
@@ -0,0 +1,45 @@
+// https://doc.rust-lang.org/std/sync/struct.Mutex.htm
+
+use std::sync::Mutex;
+
+fn main() {
+    let mutex = Mutex::new(42);
+    println!("mutex: {:?}", mutex);
+
+    {
+        println!("");
+        // pub fn lock(&self) -> LockResult<MutexGuard<T>>
+        // LockResult is a Result type
+        let lock_result = mutex.lock();
+
+        // Extract the value from the LockResult
+        let mutex_guard = lock_result.unwrap();
+
+        // Access the member within the MutexGuard using the Deref and DerefMut traits.
+        let result = *mutex_guard;
+        println!("result: {}", result);
+        println!("mutex: {:?}", mutex); // Data is locked!
+    } // Mutex is unlocked when the mutex guard (lock result?) goes out of scope.
+
+    {
+        println!("");
+        let value = *mutex.lock().unwrap();
+        println!("value: {}", value);
+    }
+
+    {
+        println!("");
+        let lock_result = mutex.lock();
+        let mut mutex_guard = lock_result.unwrap();
+        *mutex_guard += 1;
+        println!("mutex: {:?}", mutex); // Data is locked!
+    }
+    // This must lock and unlock the mutex internally
+    println!("mutex: {:?}", mutex);
+
+    {
+        println!("");
+        *mutex.lock().unwrap() += 1;        // Lock goes out of scope on this line
+        println!("mutex: {:?}", mutex);
+    }
+}
