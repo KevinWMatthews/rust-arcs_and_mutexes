8bed929e4182c10da623368cba51e7fbfd5f3896
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-08 09:00:38 -0700

    Expand copy into thread example

--- src/bin/arc_thread_copy.rs
+++ src/bin/arc_thread_copy.rs
@@ -12,10 +12,21 @@ fn main() {
         println!("arc1: {:?}", arc1);
         *arc1.lock().unwrap() += 1;
     });
-    let handle2 = thread::spawn(move || {
+    // println!("arc1: {:?}", arc1);
+
+    // The `move` keyword forces references to be moved into the closure.
+    // If `move` is not specified, the borrow checker fails:
+    // the closure only uses references and this thread may outlive the main thread,
+    // which causes a dangling reference.
+    // We can manually move the arc into the thread.
+    let handle2 = thread::spawn(|| {
+        // This is why `move` is used
+        let arc2 = arc2;
+
         println!("arc2: {:?}", arc2);
         *arc2.lock().unwrap() += 2;
     });
+    // println!("arc2: {:?}", arc2);
 
     handle1.join().unwrap();
     handle2.join().unwrap();
