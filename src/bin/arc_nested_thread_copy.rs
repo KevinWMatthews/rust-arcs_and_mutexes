b85aded7d6be9c336cd60a2fc491f57f63583f3b
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-08 08:45:52 -0700

    Pass an arc to a nested thread

---
+++ src/bin/arc_nested_thread_copy.rs
@@ -0,0 +1,30 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    let arc = Arc::new(Mutex::new(0));
+
+    // Could clone only once if we don't need to access the shared data again
+    let arc1 = Arc::clone(&arc);
+    let arc2 = Arc::clone(&arc);
+
+    let handle1 = thread::spawn(move || {
+        println!("arc1: {:?}", arc1);
+        let subarc = Arc::clone(&arc1);
+        let subhandle = thread::spawn(move || {
+            println!("subarc: {:?}", subarc);
+            *subarc.lock().unwrap() -= 7;
+        });
+        *arc1.lock().unwrap() += 1;
+        subhandle.join().unwrap();
+    });
+    let handle2 = thread::spawn(move || {
+        println!("arc2: {:?}", arc2);
+        *arc2.lock().unwrap() += 2;
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
+    println!("arc: {:?}", arc);
+}
