820db4ff11669b8936dbf08b7b7cae718d1eb017
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-09-08 08:56:45 -0700

    Add comments to nested thread copy

--- src/bin/arc_nested_thread_copy.rs
+++ src/bin/arc_nested_thread_copy.rs
@@ -10,11 +10,15 @@ fn main() {
 
     let handle1 = thread::spawn(move || {
         println!("arc1: {:?}", arc1);
+
         let subarc = Arc::clone(&arc1);
+        // Experiment with remove the 'move' keyword -
+        // Then the borrowed value doesn't live long enough
         let subhandle = thread::spawn(move || {
             println!("subarc: {:?}", subarc);
             *subarc.lock().unwrap() -= 7;
         });
+
         *arc1.lock().unwrap() += 1;
         subhandle.join().unwrap();
     });
